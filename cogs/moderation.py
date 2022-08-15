75748b43fd2b3caee7c393bd96676196df447e00
Raj Sharma <yrsharma@icloud.com>
2022-08-15 13:48:31 +0100

    Logging config cmd

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+
 import asyncio
 import datetime
+import json
+from itertools import zip_longest
 
 import discord
 from discord import app_commands
 from discord.ext import commands
 
 from main import Xanno
+from utils.mappings import LOGGING_CALLBACKS
 from utils.views import RevokeView
 
 
@@ -69,6 +74,62 @@ class Moderation(commands.Cog):
         if not button.disabled:
             button.disabled = True
             await resp.edit(view=view)
+            view.stop()
+
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    @commands.hybrid_group(name="logging")
+    async def logging(self, ctx: commands.Context) -> None:
+        ...
+
+    @commands.guild_only()
+    @commands.has_permissions(administrator=True)
+    @logging.command(name="config")
+    async def config(self, ctx: commands.Context) -> None | discord.Message:
+        """Configure the logging extension for your server"""
+        with open("logging.json", "r") as fp:
+            data = json.load(fp)
+
+        if not data.get(str(ctx.guild.id)):
+            config = {"channel": None, "callbacks": LOGGING_CALLBACKS}
+            data[str(ctx.guild.id)] = config
+        else:
+            config = data[str(ctx.guild.id)]
+
+        channel = (
+            None if not config["channel"] else ctx.guild.get_channel(config["channel"])
+        )
+
+        embed = discord.Embed(
+            colour=self.bot.colour,
+            title=f"Logging is {'enabled' if channel else 'disabled'} in {ctx.guild.name}",
+            description=f"**Callbacks are being redirected to {channel.mention}**\n```ansi"
+            if channel
+            else "",
+            timestamp=datetime.datetime.now(),
+        )
+
+        if channel:
+            callbacks = list(
+                zip_longest(
+                    *([iter([(k, v) for k, v in config["callbacks"].items()])] * 2),
+                    fillvalue=None,
+                )
+            )
+            for group in callbacks:
+                to_add = []
+                for item in group:
+                    if item:
+                        emoji = "✅" if item[1] else "❌"
+                        ansi = f"\u001b[{'32' if item[1] else '31'}m"
+                        to_add.append(
+                            f"{emoji}{ansi}{item[0].removeprefix('on_')}\u001b[0m"
+                        )
+                to_add[0] += " " * (34 - len(to_add[0]))
+                embed.description += "\n" + "".join(to_add)
+            embed.description += "\n```"
+
+        return await ctx.reply(embed=embed)
 
 
 async def setup(bot: Xanno) -> None:
